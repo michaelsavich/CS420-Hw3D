d49e748c44ad22b365ad4313bdf998cb5f61205c
dev <savichmichael@gmail.com>
2018-10-17 11:57:17 -0400

    initial commit

---
+++ Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../asst2.cpp \
+../glsupport.cpp \
+../ppm.cpp 
+
+OBJS += \
+./asst2.o \
+./glsupport.o \
+./ppm.o 
+
+CPP_DEPS += \
+./asst2.d \
+./glsupport.d \
+./ppm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=gnu++03 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
